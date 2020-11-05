1ddee928ec5f0bff63c9ee9f8acec70499434ff3
Bach Thanh Long <longbt@lumi.biz>
2020-11-05 21:46:23 +0700

    update

---
+++ src/renderer/components/page/account/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>HELLO</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/router/account.js
@@ -0,0 +1,33 @@
+import store from '../store'
+
+export default {
+  path: '/account',
+  name: 'account',
+  component: require('@/components/layout/Account').default,
+  children: [{
+    path: 'login',
+    name: 'account-login',
+    component: require('@/components/page/account/Login').default,
+    beforeEnter: beforeEnterLogin,
+  }]
+}
+
+async function beforeEnterLogin(to, from, next) {
+  let rememberMe = localStorage.getItem("rememberMe");
+  let refresh_token = localStorage.getItem("refresh_token");
+
+  if (rememberMe == "true") {
+    if (refresh_token && refresh_token != 'undefined') {
+      let tokenData = await store.dispatch("User/refreshToken", {
+        refresh_token
+      });
+      if (tokenData && tokenData.data) {
+        sessionStorage.setItem("access_token", tokenData.data.access_token);
+        localStorage.setItem("refresh_token", tokenData.data.refresh_token);
+
+        next({name: "listHome"});
+      }
+    }
+  }
+  next()
+}
