1ec1c498087a0a60ebb86b51a90f4c93fdad0796
BLong1010 <longbt@hamsa.co>
2020-12-22 17:32:54 +0700

    fix bug

--- src/renderer/plugins/argon-kit.js
+++ src/renderer/plugins/argon-kit.js
@@ -1,3 +1,12 @@
+import "@/assets/vendor/nucleo/css/nucleo.css";
+import "@/assets/vendor/font-awesome/css/font-awesome.css";
+import "@/assets/vendor/MaterialDesign/scss/materialdesignicons.scss";
+import "@/assets/utm-avo/style.css";
+// import "@/assets/icomoon/style.css";
+import "@/assets/icomoon2/style.css";
+import "@/assets/scss/argon.scss";
+
+
 import config from "../connect/config";
 
 import globalComponents from "./globalComponents";
